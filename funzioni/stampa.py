3f4e458a55830d7b2e46f2c59dc938ca302f76b1
micheleavella <michele.avella.98@gmail.com>
2023-12-10 18:30:23 +0100

    data consegna

--- funzioni/stampa.py
+++ funzioni/stampa.py
@@ -1,22 +1,25 @@
-from openpyxl import load_workbook
 import os
 
-def stampa(cliente, data, quantità, capo, commento):
+from openpyxl import load_workbook
+
+
+def stampa(cliente, data, quantità, capo, commento, data_consegna):
     workbook = load_workbook(filename="dati/template.xlsx")
     sheet = workbook.active
 
     sheet["A4"].value = cliente.upper()
     sheet["A5"].value = data.strftime("%d/%m/%Y")
+    sheet["C6"].value = data_consegna.strftime("%d/%m/%Y")
 
     for i in range(len(capo)):
-        sheet["A" + str(7 + i)].value = quantità[i]
-        sheet["B" + str(7 + i)].value = capo[i].upper()
-        sheet["C" + str(7 + i)].value = commento[i].upper()
-        
+        sheet["A" + str(8 + i)].value = quantità[i]
+        sheet["B" + str(8 + i)].value = capo[i].upper()
+        sheet["C" + str(8 + i)].value = commento[i].upper()
+
     for i in range(len(capo), 100):
-        sheet["A" + str(7 + i)].value = None
-        sheet["B" + str(7 + i)].value = None
-        sheet["C" + str(7 + i)].value = None
+        sheet["A" + str(8 + i)].value = None
+        sheet["B" + str(8 + i)].value = None
+        sheet["C" + str(8 + i)].value = None
 
     workbook.save("dati/out.xlsx")
     os.startfile("dati\\out.xlsx", "print")
